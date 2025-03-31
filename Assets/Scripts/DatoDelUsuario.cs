e67dcdaa9bafc53c12b4f7df18b906df57399764
facu-pirolo <49518308@est.ort.edu.ar>
2025-03-31 10:03:53 -0300

    puesto nombre altura etc

--- Assets/Scripts/DatoDelUsuario.cs
+++ Assets/Scripts/DatoDelUsuario.cs
@@ -8,12 +8,12 @@ public class DatoDelUsuario : MonoBehaviour
     public string nombreUsuario;
     public int edadUsuario;
     public float alturaUsuario;
-    public bool esDonante
+    public bool esDonante;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Mi nombre es " + nombreUsuario + ", tengo " + edadUsuario + ", mido " + alturaUsuario + " y es " + esDonante + " que soy donante de organos.");
     }
 
     // Update is called once per frame
