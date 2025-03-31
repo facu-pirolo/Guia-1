afa52371c3f4ec2a9f58c818f3c04156c51e75de
facu-pirolo <49518308@est.ort.edu.ar>
2025-03-31 10:41:01 -0300

    ejercicios 5 y 6 realizados

---
+++ Assets/Scripts/SumaYProducto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumaYProducto : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        num1 = 4;
+        num2 = 5;
+        Debug.Log(num1 + num2);
+        Debug.Log(num1 * num2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
