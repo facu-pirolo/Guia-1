112e225a11ba949687e64bb3edd55d857e174fab
facu-pirolo <49518308@est.ort.edu.ar>
2025-04-03 10:00:03 -0300

    ejercicio 7 de division prohibida hecho

---
+++ Assets/Scripts/DivisionProhibida.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DivisionProhibida : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    float resultado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num2 == 0)
+        {
+            //instrucciones por true
+            Debug.Log("No se puede dividir por 0");
+        } else
+        {
+            //instrucciones por false
+            resultado = num1 / num2;
+            Debug.Log(num1 + " / " + num2 + " = " + resultado);
+        }
+      
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
