0656c2da3f5bf1331f2f071907d8ab8210d942a3
facu-pirolo <49518308@est.ort.edu.ar>
2025-04-03 10:38:22 -0300

    ejercicio estacionamiento hecho

---
+++ Assets/Scripts/Estacionamiento.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Estacionamiento : MonoBehaviour
+{
+    public int horaActual;
+    bool estaAbierto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (horaActual < 0 || horaActual > 24)
+        {
+            Debug.Log("Ha ingresado una hora incorrecta");
+            return;
+        }else if (horaActual >= 10 && horaActual < 18)
+        {
+            estaAbierto = true;
+            
+        } else
+        {
+            estaAbierto = false;
+           
+        }
+        Debug.Log("El estacionamiento esta abierto" + estaAbierto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
