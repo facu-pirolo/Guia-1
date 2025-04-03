c0f2e66de791e7904c9fd6849f7364c594aa8e95
facu-pirolo <49518308@est.ort.edu.ar>
2025-04-03 10:15:58 -0300

    Ejercicio 8 Par o Impar hecho

---
+++ Assets/Scripts/ParOImpar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParOImpar : MonoBehaviour
+{
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num1 > 0)
+        {
+            if (num1 % 2 == 0)
+            {
+                Debug.Log("El numero es par");
+            }
+            else
+            {
+                Debug.Log("El numero es impar");
+            }
+        }
+        else
+        {
+            Debug.Log("El numero debe ser mayor que cero");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
