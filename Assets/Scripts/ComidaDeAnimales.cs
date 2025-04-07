b8624e81bac6979d782499fdea36ec7a1e39da5e
facu-pirolo <49518308@est.ort.edu.ar>
2025-04-07 10:40:37 -0300

    comida animales primera parte

---
+++ Assets/Scripts/ComidaDeAnimales.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComidaDeAnimales : MonoBehaviour
+{
+    public string codigoAnimal;
+    public int diasAnimal;
+    public int cantidadAlimento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (codigoAnimal == "PP")
+        {
+            cantidadAlimento = 400;
+        } else if (codigoAnimal == "PG")
+        {
+            cantidadAlimento = 700;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
